6257c075523c0c2ff2b301a549aac66610dfb21a
tinakralimarkova1 <67720045+tinakralimarkova1@users.noreply.github.com>
2026-03-22 13:44:37 +0800

    Update TSVtoCSV.py

--- TSVtoCSV.py
+++ TSVtoCSV.py
@@ -3,6 +3,6 @@ import csv
 with open('JMD_satcat.tsv', 'r', newline='', encoding='utf-8') as tsv_file:
     tsv_reader = csv.reader(tsv_file, delimiter='\t')
     
-    with open('JMD_satcat.csv', 'w', newline='', encoding='utf-8') as csv_file:
+    with open('JMD_satcat2.csv', 'w', newline='', encoding='utf-8') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerows(tsv_reader)
